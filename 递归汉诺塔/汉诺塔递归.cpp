f9f076b445d499e7682935c9ff279b269232de14
The coder <xmj15715216140@163.com>
2017-12-20 17:30:48 +0800

    -

--- 递归汉诺塔/汉诺塔递归.cpp
+++ 递归汉诺塔/汉诺塔递归.cpp
@@ -1,26 +1,22 @@
 #include <stdio.h>
-int count = 0; 
-int fno(int n ,char A ,char B ,char C);
-int main()
-{
-	int n;
-	scanf("%d",&n);
-	fno(n,'A','B','C');
- }
-int fno(int n, char A ,char B, char C)
-  {
+int count = 0;
+int fno(int n, char A ,char B, char C) {
   	if (n==1)
-  	{
-  		count++;
-  		printf("%03d  %c-->%c\n",count, A, C);
- 	 } 
-  	else
-	  {
+  		{
+  			printf("%03d  %c-->%c\n",++count, A, C);
+ 		 } 
+  	else {
 	  	
 	  	fno(n-1,A,C,B);
-		printf("%03d  %c-->%c\n",count, A, C);
+		printf("%03d  %c-->%c\n",++count, A, C);
 	  	fno(n-1,B,A,C);
 	  }
 	  
   }
+int main() {
+	int n;
+	scanf("%d",&n);
+	fno(n,'A','B','C');
+ }
+
   
